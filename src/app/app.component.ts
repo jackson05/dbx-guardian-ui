0f9e67d5e6694077625e18e1ab38689f1f329a8d
jackson05 <jackson5g.bi@gmail.com>
2024-12-08 23:19:09 +0100

    created the side bar and memu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SidebarComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'dbx-guardian-ui';
--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-sidebar',
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
-})
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import {  MatMenuModule} from '@angular/material/menu';
+import { CustomProfileComponent } from './custom-profile/custom-profile.component';
+import { CommonModule } from '@angular/common';
+
+export type MenuItems = {
+  icon: string;
+  label: string;
+  route: string;
+};
+
+  @Component({
+    selector: 'app-sidebar',
+    standalone: true,
+    imports: [
+      MatToolbarModule,
+      MatSidenavModule,
+      MatIconModule,
+      MatListModule,
+      MatButtonModule,
+      MatMenuModule,
+      RouterOutlet,
+      CustomProfileComponent,
+      CommonModule
+
+    ],
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.css']
+
+  })
 export class SidebarComponent {
+  title="DBX Guardian"
 
+  menuItems= signal<MenuItems[]>([
+    {label: 'Dashboard', route: '/dashboard', icon: 'dashboard'},
+    {label: 'DBX Workspaces', route: '/workspaces', icon: 'folder'},
+    {label: 'Clusters', route: '/clusters', icon: 'storage'},
+    {label: 'Pipelines', route: '/pipeline', icon: 'alt_route'},
+    {label:'Notebooks', route: '/notebooks', icon: 'book'},
+    {label: 'Jobs', route: '/jobs', icon: 'build'},
+    {label: 'Migration', route: '/migration', icon: 'sync'},
+    {label: 'Settings', route: '/settings', icon: 'settings'},
+  ]
+  )
+
+  subMenuItems= signal<MenuItems[]>([
+    {label: 'Settings', route: '/settings', icon: 'settings'},
+    {label: 'Users', route: '/users', icon: 'people'},
+    {label: 'Logout', route: '/logout', icon: 'exit_to_app'}
+  ]
+  )
 }
