d334e538c56f9cb516ee064950051f67b584b45a
jackson05 <jackson5g.bi@gmail.com>
2024-12-10 01:31:17 +0100

    the router added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { DbxworkspacesComponent } from './core/dbxworkspaces/dbxworkspaces.component';
 
 export const routes: Routes = [
 
@@ -6,5 +8,17 @@ export const routes: Routes = [
     path: 'dashboard',
     pathMatch: 'full',
     redirectTo: 'dashboard'
+  },
+
+  {
+    path:'dashboard',
+    component: DashboardComponent,
+    // loadChildren: () => import('./core/dashboard/dashboard.component').then(m => m.DashboardComponent)
+    //loadChildren: () => import('./core/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: 'workspaces',
+    component: DbxworkspacesComponent
+
   }
 ];
