91e116abbb58e3f1df964b2b344d678f5d7aeddf
jackson05 <jackson5g.bi@gmail.com>
2024-12-19 03:33:09 +0100

    modification of interceptor

--- src/app/core/interceptor/token.custom.interceptor.ts
+++ src/app/core/interceptor/token.custom.interceptor.ts
@@ -7,7 +7,6 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-//import { TokenService } from './token.service'; // Service to manage the token
 
 @Injectable()
 
@@ -17,8 +16,6 @@ export class CustomInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this._keycloakService.keycloak?.token;
 
-    //this.tokenService.saveToken();
-    //const token = this.tokenService.getToken();
 
     if (token) {
       const authReq = req.clone({
